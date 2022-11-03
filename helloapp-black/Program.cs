9cad198b6a3ba0979096fb49c0052b37c7fd6e81
black <black@example.com>
2022-11-03 17:25:28 +0500

    Call GreetBlack

--- helloapp-black/Program.cs
+++ helloapp-black/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("hello everyone");
+            GreetBlack();
         }
 
         private static void GreetBlack()
