4b3944e76c8f71430d9ee0827c35f26284a4ed54
black <black@example.com>
2022-11-03 16:56:44 +0500

    Add GreetBlack

--- helloapp-black/Program.cs
+++ helloapp-black/Program.cs
@@ -7,6 +7,11 @@
             Console.WriteLine("hello everyone");
         }
 
+        private static void GreetBlack()
+        {
+            Console.WriteLine("hello black");
+        }
+
         private static void GreetWhite()
         {
             Console.WriteLine("hello white");
